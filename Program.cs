1310cd5c75d3445b8240d41fac36e6acb4e12e65
paulo2606 <paulohpereira@outlook.com.br>
2025-06-18 10:13:50 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestesDeMesa
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TesteDeMesa meuTeste = new TesteDeMesa();
+
+            meuTeste.Teste01();
+        }
+    }
+}
---
+++ TesteDeMesa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TestesDeMesa
+{
+    public class TesteDeMesa
+    {
+        private int a;
+        private int b;
+        private int c;
+        private int[] v;
+
+        public TesteDeMesa()
+        {
+            a = 10;
+            b = 20;
+            c = (a+b) / 2;
+            c = c - 40;
+            v = new int[6];
+        }
+
+        public void Teste01()
+        {
+            Console.WriteLine("Valores em cada posição do vetor:");
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (i == 3)
+                {
+                    v[i] = a + b + c;
+                    Console.WriteLine($"Vetor na posição {i} = {v[i]}");
+                }
+                else
+                {
+                    Console.WriteLine($"Vetor na posição: {i} = {v[i]}");
+                }
+            }
+            Console.WriteLine("\nValores das variaveis:");
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            Console.WriteLine($"c = {c}");
+        }
+    }
+}
